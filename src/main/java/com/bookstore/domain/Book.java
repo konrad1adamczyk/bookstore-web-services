9ff65b2bddae230b5e70fe1a67431203d06572d7
KAdamczyk <konrad1adamczyk@gmail.com>
2018-01-18 15:37:55 +0100

    31 prawie

---
+++ src/main/java/com/bookstore/domain/Book.java
@@ -0,0 +1,44 @@
+package com.bookstore.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+public class Book implements Serializable {
+    private static final long serialVersionUID = 2356345L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id", nullable = false, updatable = false)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String pablisher;
+    private String publicationDate;
+    private String language;
+    private String category;
+    private int numberOfPages;
+    private String format;
+    private String isbn;
+    private double shippingWeight;
+    private double listPrice;
+    private double ourPrice;
+    private boolean active = true;
+
+    @Column(columnDefinition="text")
+    private String description;
+    private int inStockNumber;
+
+    @Transient
+    private MultipartFile bookImage;
+
+    public Book() {
+    }
+}
